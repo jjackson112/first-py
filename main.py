dbc0e2ba1b322cbc61ebc25575e94de952546ddc
jjackson112 <jjackson11211@gmail.com>
2025-04-02 16:36:18 -0400

    Try and Except

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ elif user_input == "Chippy":
   print("Birthday is 02/31/2000")
 else:
   print("Can't find a birthday for this person.")
+
+# Try and Except
+
+print("Which two numbers do you want to add together?")
+
+user_input1 = float(input("Input your first number: "))
+user_input2 = float(input("Input your second number: "))
+
+try:
+  print(user_input1 + user_input2)
+except:
+  print("At least one of your inputs was not a number")
