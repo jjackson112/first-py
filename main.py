957d7cdaf4104480df8ed86256ee5effc23d885b
jjackson112 <jjackson11211@gmail.com>
2025-04-02 15:19:41 -0400

    Birthday Reminder

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 skillz ="Howdy, I'm busy crushing skillz!"
 print(skillz + "And I'm killing it!")
 
+user_input = input("Which birthday are you looking for? ")
+
+if user_input == "Sasha":
+  print("Birthday is 01/01/2009")
+elif user_input == "Chippy":
+  print("Birthday is 02/31/2000")
+else:
+  print("Can't find a birthday for this person.")
